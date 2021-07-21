3869673715261ada62ac40b6adf7feddfdf6ef19
Kamil Neczaj <kneczaj@gmail.com>
2021-07-22 00:11:49 +0200

    header bar

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './app.css';
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Customer points calculator
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
